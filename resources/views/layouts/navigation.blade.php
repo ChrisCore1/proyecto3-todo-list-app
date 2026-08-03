a0c0b07d6138cb7163d4d11ac28e73506d96b323
ChrisCore1 <ChrisCore1@users.noreply.github.com>
2026-08-03 14:28:31 -0400

    feat: add layout for navigation and displaying messages

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto 3 - TodoList</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('tasks.index') }}">
+                TodoList App
+            </a>
+            <div class="navbar-nav">
+                <a class="nav-link" href="{{ 'tasks.index' }}">
+                    Tareas
+                </a>
+                <a class="nav-link" href="{{ 'categories.index' }}">
+                    Categorias
+                </a>
+                <a class="nav-link" href="{{ 'tags.index' }}">
+                    Etiquetas
+                </a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+
+        @yield('content')
+    </div>
+</body>
+</html>
