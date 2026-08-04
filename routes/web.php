45ad26de9d93fa291ee1d6bd218fbb660071019d
ChrisCore1 <ChrisCore1@users.noreply.github.com>
2026-08-04 11:39:07 -0400

    feat: add ROUTE for Category

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
 Route::resource('tasks', TaskController::class);
+Route::resource('categories', CategoryController::class);
