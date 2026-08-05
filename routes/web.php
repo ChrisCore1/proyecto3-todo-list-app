1a57b4ba7e756383eb0ea4e46137f2bd02668567
ChrisCore1 <ChrisCore1@users.noreply.github.com>
2026-08-04 23:15:24 -0400

    feat: add ROUTE for Tag

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::resource('tasks', TaskController::class);
 Route::resource('categories', CategoryController::class);
+Route::resource('tags', TagController::class);
